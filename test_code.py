4e2beb4fd4a66856c7d46b8eede509a97ec75f24
wzy <zywang9691@gmail.com>
2023-05-22 22:13:08 +0800

    format code

--- test_code.py
+++ test_code.py
@@ -2,10 +2,10 @@
 
 import matplotlib.pyplot as plt
 
-import numpyas np
+import numpy as np
 
-x=np.linspace(0,20,100)
+x = np.linspace(0, 20, 100)
 
-plt.plot(x,np.sin(x))
+plt.plot(x, np.sin(x))
 
 plt.show()
